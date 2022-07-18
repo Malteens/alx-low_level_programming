2335306d8b2dec70ed8339cab8e4523ce4be0f01
malteens <ntescoboy@gmail.com>
2022-07-18 15:08:05 -0700

    main.h

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -76,4 +76,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 
 	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
 
+	return (add_strings(n1, n2, r, --size_r));
+}
